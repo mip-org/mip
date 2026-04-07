0888becd33174c54324d26fba9515555b7616750
Dan Fortunato <dan.fortunato@gmail.com>
2026-04-07 17:39:48 -0400

    Shift whitespace

--- +mip/reset.m
+++ +mip/reset.m
@@ -7,17 +7,17 @@ function reset()
 % Unloads all packages (including sticky ones, excluding mip itself) and
 % removes all MIP key-value stores from persistent storage.
 
-    % Unload all packages (force mode: includes sticky packages)
-    mip.unload('--all', '--force');
+% Unload all packages (force mode: includes sticky packages)
+mip.unload('--all', '--force');
 
-    % Remove all MIP key-value stores
-    keys = {'MIP_LOADED_PACKAGES', 'MIP_DIRECTLY_LOADED_PACKAGES', 'MIP_STICKY_PACKAGES'};
-    for i = 1:length(keys)
-        if isappdata(0, keys{i})
-            rmappdata(0, keys{i});
-        end
+% Remove all MIP key-value stores
+keys = {'MIP_LOADED_PACKAGES', 'MIP_DIRECTLY_LOADED_PACKAGES', 'MIP_STICKY_PACKAGES'};
+for i = 1:length(keys)
+    if isappdata(0, keys{i})
+        rmappdata(0, keys{i});
     end
+end
 
-    fprintf('mip has been reset\n');
+fprintf('mip has been reset\n');
 
 end
