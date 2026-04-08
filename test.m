e59de0de71e261e31280b3c0330e18a5171dbe9f
Jeremy Magland <jmagland@flatironinstitute.org>
2026-04-08 13:41:13 -0400

    Revert "Don't rmpath tests/ after running"
    This reverts commit c32132e5253bcba384c887a33bd80b01e89d76ac.

--- test.m
+++ test.m
@@ -6,6 +6,7 @@ scriptDir = fileparts(mfilename('fullpath'));
 testsDir = fullfile(scriptDir, 'tests');
 
 addpath(testsDir);
+cleanupObj = onCleanup(@() rmpath(testsDir));
 
 results = run_tests();
 
